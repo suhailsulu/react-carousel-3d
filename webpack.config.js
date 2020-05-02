2825011dec0c40a5c9cc466acfe2b6fa5c5316e6
Suhail C <mr.suhsilsulu@gmail.com>
2020-05-02 14:58:36 +0530

    update webpack and other packages

--- webpack.config.js
+++ webpack.config.js
@@ -2,17 +2,18 @@ const webpack = require('webpack');
 // const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const pkg = require('./package.json');
 const path = require('path');
-const libraryName= "Carousal";
+const libraryName = "Carousal";
 
 module.exports = {
+  mode:'production',
   entry: path.join(__dirname, "./src/index.js"),
   output: {
-    path: path.join(__dirname, './dist'),      
-    filename: 'index.js',      
-    library: libraryName,      
-    libraryTarget: 'umd',      
-    publicPath: '/dist/',      
-    umdNamedDefine: true 
+    path: path.join(__dirname, './dist'),
+    filename: 'index.js',
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
   },
   module: {
     rules: [
@@ -23,44 +24,44 @@ module.exports = {
         use: {
           loader: 'babel-loader',
           options: {
-            presets: ['env']
+            presets: ['@babel/preset-env']
           }
         }
       },
       {
         test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
+        use: ['style-loader', 'css-loader']
       },
       {
         test: /\.scss$/,
         use: [{
-            loader: "style-loader" // creates style nodes from JS strings
+          loader: "style-loader" // creates style nodes from JS strings
         }, {
-            loader: "css-loader" // translates CSS into CommonJS
+          loader: "css-loader" // translates CSS into CommonJS
         }, {
-            loader: "sass-loader" // compiles Sass to CSS
+          loader: "sass-loader" // compiles Sass to CSS
         }]
       }
     ]
   },
   externals: {
-    react: {          
-      commonjs: "react",          
-      commonjs2: "react",          
-      amd: "React",          
-      root: "React"      
-    },      
-    "react-dom": {          
-      commonjs: "react-dom",          
-      commonjs2: "react-dom",          
-      amd: "ReactDOM",          
-      root: "ReactDOM"      
-    }  
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM"
+    }
   },
-  resolve: {      
-    alias: {          
+  resolve: {
+    alias: {
       'react': path.resolve(__dirname, './node_modules/react'),
-      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),      
-    }  
-},
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
+    }
+  },
 };
